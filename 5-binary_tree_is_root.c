d93452c0d5999082602d878c694519ffa5ccdf0e
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-11-16 08:50:40 +0200

    [Fix] Correct betty style errors

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,7 +1,10 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_is_root - check if the node is root
+ * @node: the node to check for root
  *
+ * Return: 1 if root, 0 if not
  */
 
 int binary_tree_is_root(const binary_tree_t *node)
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,14 +2,6 @@
 
 
 /**
- * max - find the maximum between two numbers
- * @num1: first number
- * @num2: second number
- *
- * Return: the greater between num1 and num2
- */
-
-/**
  * binary_tree_height - measures the height of a binary tree
  * @tree:  pointer to the root node of the tree to measure the height.
  *
@@ -31,7 +23,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		left_height = binary_tree_height(tree->left);
 		right_height = binary_tree_height(tree->right);
-		
+
 		if (left_height >= right_height)
 			return (left_height + 1);
 		else
