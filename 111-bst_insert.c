749a9c4897d293354483c82a88edfd26af3f9e09
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-11-16 15:49:04 +0200

    [task 25] BST insert

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,51 +1,48 @@
 #include "binary_trees.h"
 
 /**
- * bst_insert_util - inserts a value in a Binary Search Tree
- * @root: double pointer to the root node of the BST to insert the value
- * @node: the node containing the value to be inserted
- *
- * Return: pointer to the created node, or NULL on failure
+ * bst_insert - inserts a value in a Binary Search Tree
+ * @tree: a double pointer to the root node of the BST to insert the value
+ * @value: the value to store in the node to be inserted
+ * Return: A pointer to the created node
+ *         NULL on failure
  */
-
-bst_t *bst_insert_util(bst_t *root, bst_t *node)
-{
-	if (!root)
-	{
-		root = node;
-		return (node);
-	}
-
-	else if (root->n < node->n && !root->right)
-	{
-		node->parent = root;
-		root->right = node;
-		return (node);
-	}
-	else if (root->n > node->n && !root->left)
-	{
-		node->parent = root;
-		root->left = node;
-		return (node);
-	}
-	else {
-		return (NULL);
-	}
-
-	if (root->n < node->n)
-		return (bst_insert_util(root->right, node));
-	if (root->n > node->n)
-		return (bst_insert_util(root->left, node));
-
-	return (NULL);
-}
-
-/**
- *
- */
-
 bst_t *bst_insert(bst_t **tree, int value)
 {
-	bst_t *node = binary_tree_node(*tree, value);
-	return (bst_insert_util(*tree, node));
+	bst_t *tmp = NULL;
+	bst_t *second = NULL;
+	bst_t *new = NULL;
+
+	if (!tree)
+		return (NULL);
+	if (*tree == NULL)
+		return (*tree = binary_tree_node(NULL, value));
+
+	tmp = *tree;
+	while (tmp)
+	{
+		second = tmp;
+		if (value < tmp->n)
+			tmp = tmp->left;
+		else if (value > tmp->n)
+			tmp = tmp->right;
+		else if (value == tmp->n)
+			return (NULL);
+	}
+
+	new = binary_tree_node(NULL, value);
+	if (second == NULL)
+		second = new;
+	else if (value < second->n)
+	{
+		second->left = new;
+		new->parent = second;
+	}
+	else
+	{
+		second->right = new;
+		new->parent = second;
+	}
+
+	return (new);
 }
