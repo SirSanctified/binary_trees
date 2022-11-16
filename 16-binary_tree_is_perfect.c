f7017ad12f00a8d9abd9281f6f3e853d28eec255
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-11-16 09:50:43 +0200

    [task 16 & 17] Check if tree is perfect & find sibling of node

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,79 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - checks if binary tree is perfect.
+ * @tree: pointer to the root of the tree.
+ * Return: 1 if binary tree is perfect or 0 in otherwise.
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if ((binary_tree_is_full(tree) == 1) &&
+			(binary_tree_balance(tree) == 0))
+		return (1);
+	return (0);
+}
+
+
+/**
+ * binary_tree_is_full -checks if a binary tree is full
+ * @tree: pointer to the root node of the tree to check
+ *
+ * Return: 1 if true otherwise 0
+ */
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	if (!tree->left && !tree->right)
+		return (1);
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left) &&
+				binary_tree_is_full(tree->right));
+	return (0);
+}
+
+/**
+ * binary_tree_height - gets the height of a binary tree
+ * @tree: root node to draw height from for tree
+ * Return: size_t representing height, 0 on failure or NULL.
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_size = 0;
+	size_t right_size = 0;
+
+	if (!tree)
+		return (0);
+
+	left_size += 1 + binary_tree_height(tree->left);
+	right_size += 1 + binary_tree_height(tree->right);
+	if (left_size > right_size)
+		return (left_size);
+
+	return (right_size);
+}
+
+/**
+ * binary_tree_balance - find the balancing factor for the tree
+ * @tree: pointer to root of the tree to find the balancing factor
+ *
+ * Return: the tree's balancing factor
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	size_t left_height = 0, right_height = 0;
+
+	if (!tree)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	return (left_height - right_height);
+}
+
---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - finds the sibling of a node.
+ * @node: node to find sibling for.
+ * Return: pointer to sibling node.
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	else
+		return (node->parent->left);
+}
